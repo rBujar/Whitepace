0eb804fd30cfd69dcf66cef5083047e41b904c64
Bujar Roka <rokabujar@gmail.com>
2025-11-12 14:56:23 +0100

    added modifications and fixes

--- src/js/main.js
+++ src/js/main.js
@@ -3,12 +3,22 @@ import "aos/dist/aos.css";
 import Swiper from "https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.mjs";
 
 const hamburger = document.querySelector(".header__hamburger");
-const headerMenu = document.querySelector(".header__container");
+const headerMenu = document.querySelector(".header__nav");
+const menuItem = document.querySelector(".header__item");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   headerMenu.classList.toggle("active");
   document.body.classList.toggle("no-scroll");
+});
+
+menuItem.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  headerMenu.classList.toggle("active");
+  headerMenu.style.transition = "transform 0.1s ease-out";
+  if (window.innerWidth < 1200) {
+    document.body.classList.toggle("no-scroll");
+  }
 });
 
 const swiper = new Swiper(".swiper-plans", {
@@ -58,4 +68,14 @@ AOS.init({
   duration: 700,
   offset: 50,
   once: true,
+});
+
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
 });
